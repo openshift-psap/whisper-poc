0600dbfbff4496efac4d7e593f705bb8928b5cfa
NickLucche <nlucches@redhat.com>
2025-02-06 18:25:08 +0000

    fix openai sync client

--- containers/scripts/run_vllm_online.py
+++ containers/scripts/run_vllm_online.py
@@ -6,7 +6,7 @@ import os
 from typing import List, Tuple, AsyncGenerator
 from statistics import mean, median
 import asyncio
-from openai import OpenAI
+from openai import OpenAI, AsyncOpenAI
 from statistics import mean, median
 import io
 from scipy.io.wavfile import write
@@ -19,11 +19,12 @@ from transformers import AutoTokenizer
 # as well as batching
 
 # Configuration
-TOTAL_REQUESTS = 32
-MAX_REQ_CONCURRENCY = 32
+TOTAL_REQUESTS = 64
+concurrency_levels = [1, 2, 4, 8, 16, 32]#, 64, 128, 256]
 
 openai_api_base = "http://localhost:8000/v1"
-client = OpenAI(api_key="EMPTY", base_url=openai_api_base)
+# client = OpenAI(api_key="EMPTY", base_url=openai_api_base)
+client = AsyncOpenAI(api_key="EMPTY", base_url=openai_api_base)
 model_name = "openai/whisper-large-v3"  # ["openai/whisper-small", "openai/whisper-large-v3-turbo"]
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
@@ -41,27 +42,26 @@ async def transcribe_audio(client, y, sr):
         # Send loaded audio directly instead of loading from disk, dont account for that time though
         with convert_to_bytes(y, sr) as f:
             start_time = time.perf_counter()
-            transcription = client.audio.transcriptions.create(
+            transcription = await client.audio.transcriptions.create(
                 file=f,
                 model=model_name,
                 language="en",
                 # max_tokens=200, TODO cant set here
                 temperature=0.0,
             ) 
+            end_time = time.perf_counter()
             # NOTE there's no streaming in transcriptions, can't measure ttft 
     except Exception as e:
         print(f"Error: {e}")
         status = 500
-    end_time = time.perf_counter()
-    latency = end_time - start_time
     assert status==200
+    latency = end_time - start_time
     num_output_tokens = len(tokenizer(transcription.text, add_special_tokens=False).input_ids)
     return latency, num_output_tokens
 
 async def bound_transcribe(sem, client, audio):
     # Use semaphore to limit concurrent requests.
     async with sem:
-        # return await asyncio.to_thread(transcribe_audio, client, *audio)
         return await transcribe_audio(client, *audio)
 
 async def run_load_test(data, concurrent_request):
@@ -73,7 +73,7 @@ async def run_load_test(data, concurrent_request):
         tasks.append(task)
     return await asyncio.gather(*tasks)
 
-def analyze_results(results):
+def analyze_results(results, total_time):
     latencies = [latency for latency, _ in results]
     total_tokens = sum([nt for _, nt in results])
 
@@ -84,9 +84,8 @@ def analyze_results(results):
     print(f"Median Latency: {median(latencies):.4f} seconds")
     print(f"95th Percentile Latency: {sorted(latencies)[int(len(latencies) * 0.95) - 1]:.4f} seconds")
     # Throughput
-    total_time = sum(latencies)
-    throughput = len(latencies) / total_time 
-    print(f"Estimated Throughput: {throughput:.2f} requests/s")
+    req_throughput = len(latencies) / total_time 
+    print(f"Estimated req_Throughput: {req_throughput:.2f} requests/s")
     throughput = total_tokens / total_time 
     print(f"Estimated Throughput: {throughput:.2f} tok/s")
 
@@ -99,9 +98,12 @@ if TOTAL_REQUESTS > 0:
 
 # Warmup, the first librosa.load is really slow
 _ = asyncio.run(run_load_test(data_subset.select(range(2)), 1))
-start = time.perf_counter()
-results = asyncio.run(run_load_test(data_subset, MAX_REQ_CONCURRENCY))
-end = time.perf_counter()
-total_time = end - start
-print(f"Total Test Time: {total_time:.4f} seconds")
-analyze_results(results)
+
+for cl in concurrency_levels:
+    print(f"Running with concurrency level {cl}..")
+    start = time.perf_counter()
+    results = asyncio.run(run_load_test(data_subset, cl))
+    end = time.perf_counter()
+    total_time = end - start
+    print(f"Total Test Time: {total_time:.4f} seconds")
+    analyze_results(results, total_time)
